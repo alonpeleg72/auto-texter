6cc97684474a983b0fd44a3ca57aafe0db137ff1
Alon Peleg <alonpeleg72@gmail.com>
2026-04-03 20:59:27 +0300

    cleaner code

--- auto texter.py
+++ auto texter.py
@@ -27,6 +27,7 @@ def group_chat_open():
             "shutes": "https://web.whatsapp.com/accept?code=BhuaH34bpnLKoF8cmQ2mX1",
             "quotes": "https://web.whatsapp.com/accept?code=HuFgVZG405jB7ZqCOwqKAR",
             "yearA": "https://web.whatsapp.com/accept?code=GezyfprTPOx2KsHPUETP7Z"}
+    
     print(f"the groups are: {groups.keys()}")
     selected_group = input("Enter one of the groups: ")
     webbrowser.open(groups[selected_group])
@@ -34,10 +35,12 @@ def group_chat_open():
 def ANNOYING_TIME():
     choice =  input("do you want to send a message to a group or a person?").lower()
     num_messages = int(input("how much messages do you want to send?"))
+    
     match choice:
         case "group":
             message=input("Enter the message you want to send: ")
             name = input("Enter the Username of the person you want to text in said group")
+           
             group_chat_open()
             time.sleep(15)
             for i in range(num_messages):
@@ -45,6 +48,7 @@ def ANNOYING_TIME():
                 messenger(message, name)
             time.sleep(2)
             pyautogui.hotkey('ctrl', 'w')
+        
         case "person":
             message = person_chat_open()
             time.sleep(10)
@@ -59,6 +63,7 @@ def group_messenger():
     message = input("Enter the message you want to send: ")
     group_chat_open()
     time.sleep(15)
+    
     messenger(message, name)
     time.sleep(1)
     pyautogui.hotkey('ctrl', 'w')
@@ -66,6 +71,7 @@ def group_messenger():
 def person_messenger():
     message = person_chat_open()
     time.sleep(12)
+    
     messenger(message, None)
     time.sleep(1)
     pyautogui.hotkey('ctrl', 'w')
